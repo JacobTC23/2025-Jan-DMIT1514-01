f97b2576c1a9960908d292f3343f75d125805ce3
cnobert <cnobert@nait.ca>
2025-01-19 16:28:19 -0700

    big reorg

---
+++ lesson03_draw_sprite/DisplaySprite.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace lesson03_draw_sprite;
+
+public class DisplaySprite : Game
+{
+    private const int _WindowWidth = 640;
+    private const int _WindowHeight = 320;
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Texture2D _backgroundImage;
+    private Texture2D _beetleImage;
+
+    private float _x = 0;
+    private float _y = 0;
+
+    public DisplaySprite()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        _graphics.PreferredBackBufferWidth = _WindowWidth;
+        _graphics.PreferredBackBufferHeight = _WindowHeight;
+        _graphics.ApplyChanges();
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _backgroundImage = Content.Load<Texture2D>("Station");
+        _beetleImage = Content.Load<Texture2D>("Beetle");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        _x = _x + 1; //or _x++ or _x += 1
+        _y = _y + 1;
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(_backgroundImage, Vector2.Zero, Color.White);
+        _spriteBatch.Draw(_beetleImage, new Vector2(_x, _y), Color.White);
+        
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
---
+++ lesson04_animation/AnimationGame.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace lesson04_animation;
+
+public class AnimationGame : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private CelAnimationSequence _personWalking;
+    private CelAnimationPlayer _animationPlayer;
+
+    public AnimationGame()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        Texture2D spriteSheet = Content.Load<Texture2D>("Walking");
+        //pass the constructor the spritesheet, the width of each cel, 
+        //and the amount of time to display each cell
+        _personWalking = new CelAnimationSequence(spriteSheet, 81, 1 / 8f);
+        
+        _animationPlayer = new CelAnimationPlayer();
+        //start animating!
+        _animationPlayer.Play(_personWalking);
+
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        _animationPlayer.Update(gameTime);
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+        _animationPlayer.Draw(_spriteBatch, Vector2.Zero, SpriteEffects.None);
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
